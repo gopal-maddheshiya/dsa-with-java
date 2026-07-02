860c4231794376d6326183b6c308a4a4a70a9c93
Gopal Maddheshiya <gopalmaddheshiya138@gmail.com>
2026-07-02 23:45:20 +0530

    Practice string problems

---
+++ src/com/gopal/string/Basics.java
@@ -0,0 +1,17 @@
+package com.gopal.string;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        // String Declaration
+        String str = "abc";
+        String str2 = new String("xyz");
+
+        // String length
+        System.out.println(str2.length());
+
+        // Concatenation in string
+        System.out.println(str + str2);
+
+    }
+}
---
+++ src/com/gopal/string/StringFunctionCompare.java
@@ -0,0 +1,30 @@
+package com.gopal.string;
+
+public class StringFunctionCompare {
+    public static void main(String[] args) {
+        String str1 = "Tony";
+        String str2 = "Tony";
+        String str3 = new String("Tony");
+
+        // First Comparison
+        if (str1 == str2) {
+            System.out.println("Equal String");
+        } else {
+            System.out.println("Not Equal");
+        }
+
+        // Second Comparison
+        if (str1 == str3) {
+            System.out.println("Equal String");
+        } else {
+            System.out.println("Not Equal");
+        }
+
+        // Third Comparison
+        if (str1.equals(str3)) {
+            System.out.println("Equal String");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
