5c3896bd884185692d18d8d504b461a586fea93c
Gopal Maddheshiya <gopalmaddheshiya138@gmail.com>
2026-07-28 22:48:48 +0530

    Understand bit manipulation concepts

---
+++ src/com/gopal/basics/ClearIthBit.java
@@ -0,0 +1,12 @@
+package com.gopal.basics;
+
+public class ClearIthBit {
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearIthBit(10, 1));
+    }
+}
---
+++ src/com/gopal/oops/classandobject/ClassAndObject2.java
@@ -0,0 +1,28 @@
+package com.gopal.oops.classandobject;
+
+public class ClassAndObject2 {
+    public static void main(String[] args) {
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "gopalm466";
+        System.out.println(myAcc.username);
+        myAcc.setPass("adhyjghjsdf");
+        System.out.println(myAcc.getPass());
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPass(String pass) {
+        if (pass.length() >= 8) {
+            password = pass;
+        } else {
+            System.out.println("Password is too short");
+        }
+    }
+
+    public String getPass() {
+        return password;
+    }
+}
