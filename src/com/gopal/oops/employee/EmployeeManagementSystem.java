76c5e9a422ff089b57a62590ee12843986e6c7ab
Gopal Maddheshiya <gopalmaddheshiya138@gmail.com>
2026-08-02 16:59:54 +0530

    Practice method overriding and create Employee Management System

---
+++ src/com/gopal/oops/employee/EmployeeManagementSystem.java
@@ -0,0 +1,50 @@
+package com.gopal.oops.employee;
+
+abstract class Employee {
+    abstract void raiseSalary();
+
+    abstract void promote();
+}
+
+class Manager extends Employee {
+    @Override
+    void raiseSalary() {
+        System.out.println("Manager salary raised.");
+    }
+
+    @Override
+    void promote() {
+        System.out.println("Manager promoted to Senior Manager.");
+    }
+}
+
+class Engineer extends Employee {
+    @Override
+    void raiseSalary() {
+        System.out.println("Engineer sallay raised with bonus.");
+    }
+
+    @Override
+    void promote() {
+        System.out.println("Engineer promoted to Senior Engineer.");
+    }
+}
+
+public class EmployeeManagementSystem {
+    public static void main(String[] args) {
+
+        Employee[] employees = {new Manager(), new Engineer()};
+
+        System.out.println("-----Raising Salary-----");
+        for (Employee e : employees) {
+            e.raiseSalary();
+        }
+
+        System.out.println("\n-----Promotions-----");
+
+        for (Employee e : employees) {
+            e.promote();
+        }
+
+    }
+}
---
+++ src/com/gopal/oops/polymorphism/MethodOverriding.java
@@ -0,0 +1,35 @@
+package com.gopal.oops.polymorphism;
+
+class Animal {
+    void move() {
+        System.out.println("Animal is moving.");
+    }
+
+    void eat() {
+        System.out.println("Animal is eating.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void move() {
+        System.out.println("Dog is running.");
+    }
+
+    void bark() {
+        System.out.println("Dog is barking.");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+
+        animal.move();
+        animal.eat();
+
+        if (animal instanceof Dog dog) {
+            dog.bark();
+        }
+    }
+}
