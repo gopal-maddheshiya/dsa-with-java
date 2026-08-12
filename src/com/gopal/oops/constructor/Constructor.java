dc2b1443c5587b5bc199947d87b63072ed931216
Gopal Maddheshiya <gopalmaddheshiya138@gmail.com>
2026-08-12 22:32:49 +0530

    Add simple recursion problem and move Constructor file to constructor folder

--- src/com/gopal/oops/constructor/Constructor.java
+++ src/com/gopal/oops/constructor/Constructor.java
@@ -1,24 +1,4 @@
-package com.gopal.basics;
-
-public class Constructor {
-    public static void main(String args[]) {
-        Student s1 = new Student();
-        s1.name = "Gopal";
-        s1.rollNo = 35;
-        s1.pass = "xyz";
-        s1.marks[0] = 100;
-        s1.marks[1] = 90;
-        s1.marks[2] = 80;
-
-        Student s2 = new Student(s1); // copy
-        s2.pass = "abcd";
-        s1.marks[2] = 100;
-
-        for (int i = 0; i < 3; i++) {
-            System.out.println(s2.marks[i]);
-        }
-    }
-}
+package com.gopal.oops.constructor;
 
 class Student {
     String name;
@@ -59,6 +39,26 @@ class Student {
     Student(int rollNo) {
         marks = new int[3];
         this.rollNo = rollNo;
+    }
+}
+
+public class Constructor {
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        s1.name = "Gopal";
+        s1.rollNo = 35;
+        s1.pass = "xyz";
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
+
+        Student s2 = new Student(s1); // copy
+        s2.pass = "abcd";
+        s1.marks[2] = 100;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        }
     }
 }
 
---
+++ src/com/gopal/recursion/recursionpractice/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+package com.gopal.recursion.recursionpractice;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static int sum(int n) {
+        // base case
+        if (n == 1)
+            return 1;
+
+        return n + (sum(n - 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+    }
+}
