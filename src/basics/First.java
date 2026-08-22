2567ca416fa7f27c926e9ef7639485b53305757f
Gopal Maddheshiya <gopalmaddheshiya138@gmail.com>
2026-08-22 14:02:57 +0530

    refactor: reorganize project structure under src

---
+++ src/basics/First.java
@@ -0,0 +1,20 @@
+package com.gopal.basics;
+
+class Student {
+    static int collegeCode = 101;
+
+    static void display() {
+        System.out.println("Hello");
+    }
+}
+
+public class First {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!");
+        System.out.println(Student.collegeCode);
+        Student.collegeCode = 105;
+        System.out.println(Student.collegeCode);
+        Student.display();
+    }
+}
